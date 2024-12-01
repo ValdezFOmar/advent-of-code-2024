478b33c053d135a6f82638ea3fbcce91c35b4f37
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-12-01 04:13:36 -0800

    feat(day01): solution for both puzzles

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,12 +1,88 @@
 use crate::{Solution, SolutionPair};
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
-///////////////////////////////////////////////////////////////////////////////
+fn number_lists(input: &str) -> (Vec<i64>, Vec<i64>) {
+    let mut left_column = Vec::new();
+    let mut right_column = Vec::new();
+
+    for line in input.lines() {
+        let mut parts = line.split_whitespace();
+        let left = parts.next().expect("a left number");
+        let right = parts.next().expect("a right number");
+
+        left_column.push(left.parse::<i64>().expect("a valid number"));
+        right_column.push(right.parse::<i64>().expect("a valid number"));
+    }
+
+    (left_column, right_column)
+}
+
+fn solution_1(input: &str) -> i64 {
+    let (mut left, mut right) = number_lists(input);
+
+    left.sort();
+    right.sort();
+
+    left.iter()
+        .zip(right)
+        .map(|pair| (pair.0 - pair.1).abs())
+        .sum()
+}
+
+fn solution_2(input: &str) -> i64 {
+    let (left, right) = number_lists(input);
+    let mut counts = HashMap::new();
+
+    for number in right {
+        let new_count = match counts.get(&number) {
+            Some(count) => count + 1,
+            None => 1,
+        };
+        counts.insert(number, new_count);
+    }
+
+    left.iter()
+        .map(|num| match counts.get(&num) {
+            Some(count) => num * count,
+            None => 0,
+        })
+        .sum()
+}
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let sol1 = match read_to_string("input/day01.txt") {
+        Ok(input) => solution_1(&input),
+        Err(_) => 0,
+    };
+    let sol2 = match read_to_string("input/day01.txt") {
+        Ok(input) => solution_2(&input),
+        Err(_) => 0,
+    };
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+";
+
+    #[test]
+    fn example_1() {
+        assert_eq!(solution_1(EXAMPLE), 11);
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(solution_2(EXAMPLE), 31);
+    }
 }
