b55e16e20166120656475ec2097a7dc8d2e7de56
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-12-03 01:18:57 -0800

    feat(day03): solution for first and second parts

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,12 +1,51 @@
 use crate::{Solution, SolutionPair};
+use regex::Regex;
 use std::fs;
 
-fn solution_1(_input: &str) -> i64 {
-    0
+fn solution_1(input: &str) -> i64 {
+    Regex::new(r"mul\((\d+),(\d+)\)")
+        .unwrap()
+        .captures_iter(input)
+        .map(|caps| {
+            let (_, [left, right]) = caps.extract();
+            let num1 = left.parse::<i64>().unwrap();
+            let num2 = right.parse::<i64>().unwrap();
+            num1 * num2
+        })
+        .sum()
 }
 
-fn solution_2(_input: &str) -> i64 {
-    0
+fn solution_2(input: &str) -> i64 {
+    let dont_instruction = "don't()";
+    let do_instruction = "do()";
+
+    let mut text = input;
+    let mut enable = true;
+    let mut total = 0;
+
+    loop {
+        let instruction = if enable {
+            dont_instruction
+        } else {
+            do_instruction
+        };
+
+        let Some(start) = text.find(instruction) else {
+            if enable {
+                total += solution_1(text);
+            }
+            break;
+        };
+
+        if enable {
+            total += solution_1(&text[..start]);
+        }
+
+        text = &text[start + instruction.bytes().len()..];
+        enable = !enable;
+    }
+
+    total
 }
 
 pub fn solve() -> SolutionPair {
@@ -24,15 +63,19 @@ pub fn solve() -> SolutionPair {
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = "";
+    const EXAMPLE_1: &str =
+        "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+    const EXAMPLE_2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn example_1() {
-        assert_eq!(solution_1(EXAMPLE), 0);
+        assert_eq!(solution_1(EXAMPLE_1), 161);
     }
 
     #[test]
     fn example_2() {
-        assert_eq!(solution_2(EXAMPLE), 0);
+        assert_eq!(solution_2(EXAMPLE_2), 48);
     }
 }
